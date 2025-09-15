e64a93d29d5a9eeb2eda49bf2cd21a38b1cce3b2
Inespaty <minespatiente@gmail.com>
2025-09-15 11:09:43 +0200

    Updated portfolio

--- script.js
+++ script.js
@@ -29,4 +29,10 @@ function showCategory(category) {
   }
 }
 
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+  navbar.classList.toggle('active');
+};
 
